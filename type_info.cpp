552b01a3dcc87487d153d7a97c9ae98cd5a0a3fa
jacobrak <155147415+jacobrak@users.noreply.github.com>
2025-09-15 23:25:08 +0200

    byte

--- type_info.cpp
+++ type_info.cpp
@@ -10,9 +10,9 @@
 // Question 2
 // char is explained above
 // string; stores and manipulates character-like objects 32 bytes
-// "" is 1 byte
-// "x" is 2 bytes
-// "da40047" is 7 bytes
+// "" is 1 byte, 1 for \0
+// "x" is 2 bytes, 1 byte for ch and 1 for \0
+// "da40047" is 7 bytes, 6 byte of ch and 1 for \0
 #include <iostream>
 using namespace std;
 
